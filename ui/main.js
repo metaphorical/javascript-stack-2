94e01a92b1ea43752bb9150560efe7950241e078
Rastko Vukasinovic <rastko@vast.com>
2016-10-23 01:23:59 +0200

    Connected ticker to Redux

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,39 @@
 import IO from 'socket.io-client';
+import { createStore } from 'redux';
+
+
+let addStatus = (statusObject) => {
+	return {
+		type: 'ADD_STATUS',
+		status: statusObject
+	};
+};
+
+let statusReducer = (state = {}, action) => {
+		switch(action.type) {
+			case 'ADD_STATUS':
+					var newState = Object.assign({}, state);
+					if(state[action.status.company_id]) {
+						if(state[action.status.company_id][action.status.driver_id]) {
+							newState[action.status.company_id][action.status.driver_id].push(action.status);
+						} else {
+							newState[action.status.company_id][action.status.driver_id] = [action.status]
+						}
+					} else {
+						newState[action.status.company_id] = {[action.status.driver_id] : [action.status]}
+					}
+					return newState;
+			break;
+
+		}
+};
+
+let store = createStore(statusReducer);
+
+//For now I am not using unsubscribe, but am making it available.
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
 
 //connecting to same port that is serving the page, so no need ot pass anything to IO
 
@@ -7,6 +42,7 @@ let socket = IO();
 socket.on('connect', () => {
 	console.log('CONNECTED!');
 });
+
 socket.on('status-ping', (data) => {
-	console.log(data);
+	store.dispatch(addStatus(data));
 });
